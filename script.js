ae12ef6d1b3ae6ed2b216cba9469adf94b4dd4dc
Shea Cronin <shealcronin@gmail.com>
2021-04-01 17:08:50 -0400

    Add active dot class and refactor code to all be inside slider module

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
 const slider = (() => {
-    const imgEl = document.querySelector('#slide');
+    const currentSlide = document.querySelector('#slide');
     const images = [];
     let currentImgIndex = 0;
+
+    (() => {
+        const addImage = (title, src, altText) => {
+            const image = { title, src, altText };
+            images.push(image);
+            return image;
+        };
+
+        addImage(
+            'spring',
+            './i/spring.jpeg',
+            'Low-angle view of a cherry blossom tree'
+        );
+        addImage(
+            'summer',
+            './i/summer.jpeg',
+            'A colorful beach ball floating in a pool'
+        );
+        addImage(
+            'fall',
+            './i/fall.jpeg',
+            'The sun shines through orange and red trees in a forest'
+        );
+        addImage(
+            'winter',
+            './i/winter.jpeg',
+            'A white fox with yellow eyes stands in a snowy landscape'
+        );
+    })();
+
     const slideTitle = document.querySelector('#slide-title');
+
     const prevBtn = document.querySelector('#prev-btn');
+    prevBtn.addEventListener('click', () => changeSlide('prev'));
     const nextBtn = document.querySelector('#next-btn');
+    nextBtn.addEventListener('click', () => changeSlide('next'));
     const playBtn = document.querySelector('#play-btn');
     playBtn.addEventListener('click', toggleAutoPlay);
+
+    const nav = (() => {
+        const navBar = document.querySelector('#nav-bar');
+        const dots = [];
+        // Add a dot to represent each image.
+        images.forEach((image) => {
+            const dot = document.createElement('button');
+            dot.classList.add('nav-dot');
+            dot.addEventListener('click', () => slider.changeSlide(image));
+            dots.push(dot);
+            navBar.appendChild(dot);
+        });
+
+        function setActiveDot(index) {
+            // Remove the active class from all other dots.
+            dots.forEach((dot) => dot.classList.remove('active-dot'));
+            // Set active dot to the current image index.
+            dots[index].classList.add('active-dot');
+        }
+
+        return { setActiveDot };
+    })();
+
     let isAutoPlaying = false;
+
     function changeSlide(destination) {
         let newImage;
         if (destination === 'next') {
@@ -28,18 +85,22 @@ const slider = (() => {
             // Set current index to the image we've switched to.
             currentImgIndex = images.indexOf(destination);
         }
-        imgEl.src = newImage.src;
+        currentSlide.src = newImage.src;
         changeSlideTitle(newImage.title);
+        nav.setActiveDot(images.indexOf(newImage));
     }
+
     function changeSlideTitle(title) {
         slideTitle.textContent = title;
     }
+
     function animateSlide(direction) {
-        imgEl.classList.add('swipe-' + direction);
+        currentSlide.classList.add('swipe-' + direction);
         setTimeout(() => {
-            imgEl.classList.remove('swipe-' + direction);
+            currentSlide.classList.remove('swipe-' + direction);
         }, 401);
     }
+
     function autoPlay() {
         if (isAutoPlaying) {
             changeSlide('next');
@@ -48,48 +109,19 @@ const slider = (() => {
             }, 5000);
         }
     }
+
     function toggleAutoPlay() {
         isAutoPlaying = !isAutoPlaying;
         playBtn.textContent = isAutoPlaying ? '||' : '▶';
         if (isAutoPlaying) autoPlay();
     }
-    return { images, changeSlide, nextBtn, prevBtn, toggleAutoPlay };
+
+    return { images, changeSlide, toggleAutoPlay };
 })();
 
-slider.nextBtn.addEventListener('click', () => slider.changeSlide('next'));
-slider.prevBtn.addEventListener('click', () => slider.changeSlide('prev'));
-
-(() => {
-    const Img = (title, src, altText) => {
-        const img = { title, src, altText };
-        slider.images.push(img);
-        return img;
-    };
-
-    Img('spring', './i/spring.jpeg', 'Low-angle view of a cherry blossom tree');
-    Img(
-        'summer',
-        './i/summer.jpeg',
-        'A colorful beach ball floating in a pool'
-    );
-    Img(
-        'fall',
-        './i/fall.jpeg',
-        'The sun shines through orange and red trees in a forest'
-    );
-    Img(
-        'winter',
-        './i/winter.jpeg',
-        'A white fox with yellow eyes stands in a snowy landscape'
-    );
-})();
-
-const navBar = document.querySelector('#nav-bar');
-slider.images.forEach((image) => {
-    const dot = document.createElement('button');
-    dot.classList.add('nav-dot');
-    dot.addEventListener('click', () => slider.changeSlide(image));
-    navBar.appendChild(dot);
-});
+const navBar2 = {
+    el: document.querySelector('#nav-bar'),
+    dots: [],
+};
 
 slider.toggleAutoPlay();
